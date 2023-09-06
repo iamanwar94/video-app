9f40cb03eb821593c09fa9f463a06bfd3185873d
anwar ahmed <muhammadanwar.symits@gmail.com>
2023-09-06 20:10:19 +0500

    channel card work in progress

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 
-const ChannelCard = () => {
+import { Videos, ChannelCard } from "./";
+import { makeApiRequest } from "../utils/Axios";
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchChannels = async () => {
+      const data = await makeApiRequest(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await makeApiRequest(
+        `search?channelId=${id}&part=snippet%2Cid&order=date`
+      );
+
+      setVideos(videosData?.items);
+    };
+
+    fetchChannels();
+  }, [id]);
+
   return (
-    <div>ChannelCard</div>
-  )
-}
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            height: "300px",
+            background:
+              "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+      </Box>
+      <Box p={2} display="flex">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
 
-export default ChannelCard
+export default ChannelDetail;
